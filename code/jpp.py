7721150136d7527b2d5902c26fd9d0194d7b3037
Andreas Faisst <afaisst@ipac.caltech.edu>
2023-03-09 16:47:11 -0800

    added style sheet

--- code/jpp.py
+++ code/jpp.py
@@ -33,29 +33,8 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 
 
-## Plotting stuff
-mpl.rcParams['font.size'] = 12
-mpl.rcParams['axes.labelpad'] = 10
-mpl.rcParams['xtick.major.pad'] = 7
-mpl.rcParams['ytick.major.pad'] = 7
-mpl.rcParams['xtick.minor.visible'] = True
-mpl.rcParams['ytick.minor.visible'] = True
-mpl.rcParams['xtick.minor.top'] = True
-mpl.rcParams['xtick.minor.bottom'] = True
-mpl.rcParams['ytick.minor.left'] = True
-mpl.rcParams['ytick.minor.right'] = True
-mpl.rcParams['xtick.major.size'] = 5
-mpl.rcParams['ytick.major.size'] = 5
-mpl.rcParams['xtick.minor.size'] = 3
-mpl.rcParams['ytick.minor.size'] = 3
-mpl.rcParams['xtick.direction'] = 'in'
-mpl.rcParams['ytick.direction'] = 'in'
-#mpl.rc('text', usetex=True)
-mpl.rc('font', family='serif')
-mpl.rcParams['xtick.top'] = True
-mpl.rcParams['ytick.right'] = True
-mpl.rcParams['hatch.linewidth'] = 1
-
+## Style sheet
+plt.style.use('main.mplstyle')
 def_cols = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
 ### DEFINITIONS #####
--- example/example.py
+++ example/example.py
@@ -10,35 +10,18 @@ from astropy.visualization.mpl_normalize import ImageNormalize
 from astropy import units as u
 
 import matplotlib.pyplot as plt
+from matplotlib import style
 import matplotlib as mpl
 
+# import JPP
 sys.path.append("../code/")
 import jpp as jpp
 
-## Plotting stuff
-mpl.rcParams['font.size'] = 12
-mpl.rcParams['axes.labelpad'] = 10
-mpl.rcParams['xtick.major.pad'] = 7
-mpl.rcParams['ytick.major.pad'] = 7
-mpl.rcParams['xtick.minor.visible'] = True
-mpl.rcParams['ytick.minor.visible'] = True
-mpl.rcParams['xtick.minor.top'] = True
-mpl.rcParams['xtick.minor.bottom'] = True
-mpl.rcParams['ytick.minor.left'] = True
-mpl.rcParams['ytick.minor.right'] = True
-mpl.rcParams['xtick.major.size'] = 5
-mpl.rcParams['ytick.major.size'] = 5
-mpl.rcParams['xtick.minor.size'] = 3
-mpl.rcParams['ytick.minor.size'] = 3
-mpl.rcParams['xtick.direction'] = 'in'
-mpl.rcParams['ytick.direction'] = 'in'
-#mpl.rc('text', usetex=True)
-mpl.rc('font', family='serif')
-mpl.rcParams['xtick.top'] = True
-mpl.rcParams['ytick.right'] = True
-mpl.rcParams['hatch.linewidth'] = 1
-
+# nice Style sheet
+plt.style.use('../code/main.mplstyle')
 def_cols = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+
 
 #######################
 
